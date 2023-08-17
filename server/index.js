1c67df781bcbe7707e3df41ea55b3e8b666f164b
Kao Saelor <saelor.kao@gmail.com>
2023-08-17 00:04:33 -0700

    packages installed server running

--- server/index.js
+++ server/index.js
@@ -16,3 +16,23 @@ app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+
+console.log("Hi Kao.  Server is Running")
+
+/* MONGOOSE SETUP */
+// const PORT = process.env.PORT || 9000;
+// mongoose
+//   .connect(process.env.MONGO_URL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(async () => {
+//     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ADD DATA ONE TIME ONLY OR AS NEEDED */
+    // await mongoose.connection.db.dropDatabase();
+    // KPI.insertMany(kpis);
+    // Product.insertMany(products);
+    // Transaction.insertMany(transactions);
+//   })
+//   .catch((error) => console.log(`${error} did not connect`));
